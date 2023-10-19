9040cd2f57dba5a36806d84a4239a7a145f652a2
Hessel van der Molen <hvandermolen@dexels.com>
2023-10-19 16:45:08 +0200

    subsys/nfc : NFC subsystem + nRF NFCT drivers + nt3h2 driver + sample code

---
+++ include/zephyr/nfc/nfc_reader.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2021, Sendrato.com, All rights reserved.
+ *
+ * This software is the confidential and proprietary information of Sendrato
+ * and is subject to the terms and conditions defined in file 'LICENSE.txt',
+ * which is part of this source code package. You shall not disclose such
+ * Confidential Information and shall use it only in accordance with the terms
+ * of the license agreement you entered into with Sendrato.
+ *
+ * 2021, Sendrato, Hessel van der Molen
+ */
+
+/**
+ * @file NFC-read subsystem
+ */
+
+#ifndef __NFC_READER_H
+#define __NFC_READER_H
+
+//todo
+
+#endif /* __NFC_READER_H */
+
