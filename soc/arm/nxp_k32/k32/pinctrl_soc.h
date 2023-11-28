744675267d0bf1149c1673b5ec49bc82dfd3ab68
Alessio Lei <alelei94@yahoo.it>
2023-11-28 13:06:59 +0100

    Fixes macros for pin types D/A/I and IOCON_PINCFG

--- soc/arm/nxp_k32/k32/pinctrl_soc.h
+++ soc/arm/nxp_k32/k32/pinctrl_soc.h
@@ -25,33 +25,33 @@ typedef uint32_t pinctrl_soc_pin_t;
 
 #define IOCON_PIO_SLEW_MASK (IOCON_PIO_SLEW0_MASK | IOCON_PIO_SLEW1_MASK)
 
-#define Z_PINCTRL_IOCON_PINCFG(node_id)						\
-	(IF_ENABLED(DT_PROP(node_id, bias_pull_down), (IOCON_PIO_MODE(0x1) |))	\
-	IF_ENABLED(DT_PROP(node_id, bias_pull_up), (IOCON_PIO_MODE(0x2) |))	\
-	IF_ENABLED(DT_PROP(node_id, drive_push_pull), (IOCON_PIO_MODE(0x3) |))	\
-	IOCON_PIO_SLEW0(DT_ENUM_IDX(node_id, slew_rate)) |			\
-	IOCON_PIO_INVERT(DT_PROP(node_id, nxp_invert)) |			\
-	IOCON_PIO_DIGIMODE(!DT_PROP(node_id, nxp_analog_mode)) |		\
-	IOCON_PIO_OD(DT_PROP(node_id, drive_open_drain)) |			\
-	IOCON_PIO_ASW1(DT_PROP(node_id, nxp_analog_alt_mode)) |			\
-	IOCON_PIO_SSEL(DT_ENUM_IDX_OR(node_id, power_source, 0)) |		\
-	IOCON_PIO_FILTEROFF(!DT_NODE_HAS_PROP(node_id, nxp_i2c_filter)) |	\
-	IOCON_PIO_ECS(DT_PROP(node_id, nxp_i2c_pullup)) |			\
-	IOCON_PIO_EGP(!DT_PROP(node_id, nxp_i2c_mode)))
+#define Z_PINCTRL_IOCON_PINCFG(node_id) \
+    (IF_ENABLED(DT_PROP(node_id, bias_pull_up), (IOCON_PIO_MODE(0x0) |)) \
+    IF_ENABLED(DT_PROP(node_id, drive_push_pull), (IOCON_PIO_MODE(0x1) |)) \
+    IF_ENABLED(DT_PROP(node_id, drive_open_drain), (IOCON_PIO_MODE(0x2) |)) \
+    IF_ENABLED(DT_PROP(node_id, bias_pull_down), (IOCON_PIO_MODE(0x3) |)) \
+    IOCON_PIO_SLEW0(DT_ENUM_IDX(node_id, slew_rate)) | \
+    IOCON_PIO_INVERT(DT_PROP(node_id, nxp_invert)) | \
+    IOCON_PIO_DIGIMODE(!DT_PROP(node_id, nxp_analog_mode)) | \
+    IOCON_PIO_FILTEROFF(!DT_NODE_HAS_PROP(node_id, nxp_i2c_filter)) | \
+    IOCON_PIO_OD(DT_PROP(node_id, drive_open_drain)) | \
+    IOCON_PIO_FSEL(DT_NODE_HAS_PROP(node_id, nxp_i2c_filter)))
 
 /* Mask for digital type pin configuration register */
-#define Z_PINCTRL_IOCON_D_PIN_MASK (IOCON_PIO_OD_MASK |				\
-	IOCON_PIO_DIGIMODE_MASK | IOCON_PIO_INVERT_MASK | IOCON_PIO_SLEW_MASK |	\
-	IOCON_PIO_MODE_MASK | IOCON_PIO_FUNC_MASK)
+#define Z_PINCTRL_IOCON_D_PIN_MASK (IOCON_PIO_FUNC_MASK | \
+		IOCON_PIO_MODE_MASK | IOCON_PIO_SLEW0_MASK | IOCON_PIO_INVERT_MASK | \
+		IOCON_PIO_DIGIMODE_MASK | IOCON_PIO_FILTEROFF_MASK | IOCON_PIO_SLEW1_MASK | \
+		IOCON_PIO_OD_MASK | IOCON_PIO_IO_CLAMP_MASK )
 
 /* Mask for analog type pin configuration register */
-#define Z_PINCTRL_IOCON_A_PIN_MASK						\
-	(Z_PINCTRL_IOCON_D_PIN_MASK | IOCON_PIO_ASW1_MASK)
+#define Z_PINCTRL_IOCON_A_PIN_MASK 0x0
 
 /* Mask for i2c type pin configuration register */
-#define Z_PINCTRL_IOCON_I_PIN_MASK (Z_PINCTRL_IOCON_D_PIN_MASK |		\
-	IOCON_PIO_SSEL_MASK | IOCON_PIO_FILTEROFF_MASK | IOCON_PIO_ECS_MASK |	\
-	IOCON_PIO_EGP_MASK )
+#define Z_PINCTRL_IOCON_I_PIN_MASK (IOCON_PIO_FUNC_MASK | \
+		IOCON_PIO_EGP_MASK | IOCON_PIO_ECS_MASK | IOCON_PIO_EHS_MASK | \
+		IOCON_PIO_INVERT_MASK | IOCON_PIO_DIGIMODE_MASK | IOCON_PIO_FILTEROFF_MASK | \
+		IOCON_PIO_FSEL_MASK | IOCON_PIO_OD_MASK | IOCON_PIO_SSEL_MASK | \
+		IOCON_PIO_IO_CLAMP_MASK )
 
 #define Z_PINCTRL_STATE_PIN_INIT(group, pin_prop, idx)				\
 	DT_PROP_BY_IDX(group, pin_prop, idx) | Z_PINCTRL_IOCON_PINCFG(group),
