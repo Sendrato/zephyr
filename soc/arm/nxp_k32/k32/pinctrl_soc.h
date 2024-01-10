5b68a3e159296b072a0ff1be4b0d086daa651110
Alessio Lei <alelei94@yahoo.it>
2024-01-10 10:40:03 +0100

    Updates K32 pinctrl to handle pin nxp,plain-mode and nxp,i2c-pin

--- soc/arm/nxp_k32/k32/pinctrl_soc.h
+++ soc/arm/nxp_k32/k32/pinctrl_soc.h
@@ -19,17 +19,13 @@ extern "C" {
 
 typedef uint32_t pinctrl_soc_pin_t;
 
-/* Other LPC55sxx parts have one analog switch */
-#define IOCON_PIO_ASW1(x) 0
-#define IOCON_PIO_ASW1_MASK 0x0
-
-#define IOCON_PIO_SLEW_MASK (IOCON_PIO_SLEW0_MASK | IOCON_PIO_SLEW1_MASK)
-
 #define Z_PINCTRL_IOCON_PINCFG(node_id) \
     (IF_ENABLED(DT_PROP(node_id, bias_pull_up), (IOCON_PIO_MODE(0x0) |)) \
-    IF_ENABLED(DT_PROP(node_id, drive_push_pull), (IOCON_PIO_MODE(0x1) |)) \
-    IF_ENABLED(DT_PROP(node_id, drive_open_drain), (IOCON_PIO_MODE(0x2) |)) \
+    IF_ENABLED(DT_PROP(node_id, nxp_repeater_mode), (IOCON_PIO_MODE(0x1) |)) \
+    IF_ENABLED(DT_PROP(node_id, nxp_plain_mode), (IOCON_PIO_MODE(0x2) |)) \
     IF_ENABLED(DT_PROP(node_id, bias_pull_down), (IOCON_PIO_MODE(0x3) |)) \
+    IF_ENABLED(DT_PROP(node_id, nxp_i2c_pin), \
+        (IF_DISABLED(DT_PROP(node_id, nxp_i2c_mode), (IOCON_PIO_EGP(0x1) |)) )) \
     IOCON_PIO_SLEW0(DT_ENUM_IDX(node_id, slew_rate)) | \
     IOCON_PIO_INVERT(DT_PROP(node_id, nxp_invert)) | \
     IOCON_PIO_DIGIMODE(!DT_PROP(node_id, nxp_analog_mode)) | \
@@ -37,16 +33,13 @@ typedef uint32_t pinctrl_soc_pin_t;
     IOCON_PIO_OD(DT_PROP(node_id, drive_open_drain)) | \
     IOCON_PIO_FSEL(DT_NODE_HAS_PROP(node_id, nxp_i2c_filter)))
 
-/* Mask for digital type pin configuration register */
+/* Mask for standard IO pins configuration register */
 #define Z_PINCTRL_IOCON_D_PIN_MASK (IOCON_PIO_FUNC_MASK | \
 		IOCON_PIO_MODE_MASK | IOCON_PIO_SLEW0_MASK | IOCON_PIO_INVERT_MASK | \
 		IOCON_PIO_DIGIMODE_MASK | IOCON_PIO_FILTEROFF_MASK | IOCON_PIO_SLEW1_MASK | \
 		IOCON_PIO_OD_MASK | IOCON_PIO_IO_CLAMP_MASK )
 
-/* Mask for analog type pin configuration register */
-#define Z_PINCTRL_IOCON_A_PIN_MASK 0x0
-
-/* Mask for i2c type pin configuration register */
+/* Mask for special i2c pins (pin 10 and 11) configuration register */
 #define Z_PINCTRL_IOCON_I_PIN_MASK (IOCON_PIO_FUNC_MASK | \
 		IOCON_PIO_EGP_MASK | IOCON_PIO_ECS_MASK | IOCON_PIO_EHS_MASK | \
 		IOCON_PIO_INVERT_MASK | IOCON_PIO_DIGIMODE_MASK | IOCON_PIO_FILTEROFF_MASK | \
